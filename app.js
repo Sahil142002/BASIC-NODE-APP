d4bfd66b4c576afc0df4eff9a1592bcb3d88c426
Sahil142002 <sahilsadhwani09@gmail.com>
2026-04-24 21:30:22 +0530

    app file changes

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
     console.log("App is running...");
-    res.json({ status: "App is running since past 15 days on port 3000" });
+    res.json({ status: "App is running since 24 hrs on port 3000" });
 });
 
 app.get("/health", (req, res) => {
     console.log("Health check hit");
-    res.json({ status: "health is perfectly fine!!!" });
+    res.json({ status: "health is perfectly not fine!!!" });
 });
 
 app.get("/hello", (req, res) => {
     console.log("Hello endpoint hit");
     console.log(`IP : ${req.headers["x-forwarded-for"] || req.socket.remoteAddress}`);
-    res.json({ message: "Hello, Respected Sir!" });
+    res.json({ message: "Hello, Everyone!" });
 });
 
 app.listen(PORT, () => {
